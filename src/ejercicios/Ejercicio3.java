f54fe127f6fff82be20384e038e31e6f455aa425
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-10 08:49:18 +0100

    Reasignación de Variables

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -7,22 +7,22 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         // ? Programa que pida un numero al usuario y dibuje un triangulo en funcion de la altura introducida
         // ^ Creamos las variables para la altura y el caracter
-        long altura;
-        final char caracter = '*';
+        short altura; // * Es short porque a partir de 1000 de altura no se puede imprimir bien por la consola
+        final char CHAR = '*'; // * Es final porque no se va a modificar una vez se le asigne un valor
 
         // ^ Creamos una variable para el retraso
-        final int retraso; // ? Funcionalidad extra
+        final short RETRASO; // ? Funcionalidad extra
 
         // ^ Creamos un Scanner en la variable read y cambiamos su formato a US
         Scanner read = new Scanner(System.in).useLocale(Locale.US);
 
         // ! Pedimos la altura al usuario
         System.out.print("Introduce la altura del triangulo: ");
-        altura = read.nextLong();
+        altura = read.nextShort();
 
         // ! Pedimos el retraso al usuario
         System.out.print("Introduce el delay (ms) ([0] para desactivarlo, [10] recomendado): ");
-        retraso = read.nextInt(); // ? Extra que he agregado
+        RETRASO = read.nextShort(); // ? Extra que he agregado
 
         // ! Multiplicamos la altura x 2
         altura *= 2; // * Esto lo hago para que el triangulo se dibuje a la altura que corresponde
@@ -31,26 +31,24 @@ public class Ejercicio3 {
         System.out.println("\n---------------------------------------\n");
 
         // ! Mediante un bucle for, dibujamos el triangulo
-        for (int i = 0; i < altura; i+=2){
+        for (short i = 0; i < altura; i+=2){
             // ! Calcularemos cuantos caracteres blancos hacen falta para centrar la fila
             for (long blank = (altura - 1); blank > i; blank-=2){
                 System.out.print(" "); // * Imprimimos el espacio en blanco
 
-                // ? Extra que le he agregado
-                try {
-                    Thread.sleep(retraso);
+                try { // ? Extra que le he agregado
+                    Thread.sleep(RETRASO);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
 
             // ! Dibujamos la fila, mientras que c sea menor que la cantidad de caracteres que se tienen que imprimir por linea
-            for (int c = 0; c < i + 1; c++){
-                System.out.print(caracter); // * Imprimimos el caracter
+            for (short c = 0; c < i + 1; c++){
+                System.out.print(CHAR); // * Imprimimos el caracter
 
-                // ? Extra que le he agregado
-                try {
-                    Thread.sleep(retraso);
+                try { // ? Extra que le he agregado
+                    Thread.sleep(RETRASO);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
