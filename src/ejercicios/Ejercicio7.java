8d5c31329aa6d92c285670d2c9ce4660aad94c0e
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-10 14:21:21 +0100

    NO puedo

---
+++ src/ejercicios/Ejercicio7.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+// ? Importamos la clase Scanner y Locale
+import java.util.Scanner; import java.util.Locale;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        // ? Programa Java el cual imprima una piramide de n filas, los numeros deberan ser consecutivos empezando por 1
+    }
+}
