b49b054287d09a36c8850719e0a0bbca1e666ee5
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-10 08:48:59 +0100

    Reasignación de Variables

--- src/ejercicios/Ejercicio2.java
+++ src/ejercicios/Ejercicio2.java
@@ -7,7 +7,8 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         // ? Programa Java que pida un numero n y diga cuantos numeros primos hay entre 1 y n
         // ^ Creamos las variables para el numero y el contador, asi como otro numero que empezara siendo 2
-        long numero, contador = 0, numero2 = 2; // ? El numero 2 sirve para ir incrementando el numero que empieza en 2 y llegar hasta el numero n
+        long contador = 0, numero2 = 2; // ? El numero 2 sirve para ir incrementando el numero que empieza en 2 y llegar hasta el numero n
+        final long NUMERO; // * Es final porque no se va a modificar una vez se le asigne un valor
 
         // ^ Crearemos otra variable que sera el contador de primos
         long contadorPrimos = 0;
@@ -17,13 +18,13 @@ public class Ejercicio2 {
 
         // ! Pedimos el numero al usuario
         System.out.print("Introduce un numero: ");
-        numero = read.nextLong(); // * Para el numero
+        NUMERO = read.nextLong(); // * Para el numero
 
         // ? Salto de Línea Triple
-        System.out.println("\n" + "---------------------------------------" + "\n");
+        System.out.println("\n---------------------------------------\n");
 
         // ! Mediante un bucle for, comprobamos si el numero es primo
-        while (numero2 < numero){ // * Mientras el numero2 sea menor que el numero
+        while (numero2 < NUMERO) { // * Mientras el numero2 sea menor que el numero
             for (long i = 2; i < numero2; i++){ // * El bucle for empieza en 2 porque 1 es divisor de todos los numeros
                 if (numero2 % i == 0){ // * Si el numero es divisible entre i, no es primo
                     contador++; // * Si el número no es primo, se suma 1 al contador
@@ -34,10 +35,10 @@ public class Ejercicio2 {
         }
 
         // ! Invertimos los no primos por los primos
-        contadorPrimos = (numero - contador) - 2; // * Se le resta 2 porque 1 y el numero n no hay que incluirlos
+        contadorPrimos = (NUMERO - contador) - 2; // * Se le resta 2 porque 1 y el numero n no hay que incluirlos
 
         // ! Mostramos el resultado al usuario
-        System.out.println("Entre 1 y " + numero + " hay " + contadorPrimos + " numeros primos");
+        System.out.println("Entre 1 y " + NUMERO + " hay " + contadorPrimos + " numeros primos");
 
         // ^ Cerramos el Scanner
         read.close();
