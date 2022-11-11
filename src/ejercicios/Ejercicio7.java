cdfc6f26243ae6012ff16e5a25073571481b1622
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-11 08:59:23 +0100

    Variables Renombradas

--- src/ejercicios/Ejercicio7.java
+++ src/ejercicios/Ejercicio7.java
@@ -7,25 +7,25 @@ public class Ejercicio7 {
     public static void main(String[] args) {
         // ? Programa Java el cual imprima una piramide de n filas, los numeros deberan ser consecutivos empezando por 1, luego 121, 12321, 1234321, etc.
         // ^ Creamos la variable para filas
-        final long FILAS;
+        final long FILAS_TOTALES;
 
         // ^ Creamos un Scanner en la variable read y cambiamos su formato a US
         final Scanner read = new Scanner(System.in).useLocale(Locale.US);
 
         // ! Pedimos el numero al usuario
         System.out.print("Introduce un numero entero positivo: ");
-        FILAS = read.nextInt();
+        FILAS_TOTALES = read.nextInt();
 
         // ? Salto de Línea Triple
         System.out.println("\n---------------------------------------\n");
 
         // ! Imprimimos la piramide
-        for (int primeraMitad = 1; primeraMitad <= FILAS; primeraMitad++) {
-            for (int segundaMitad = 1; segundaMitad <= primeraMitad; segundaMitad++) {
-                System.out.print(segundaMitad);
+        for (int filaActual = 1; filaActual <= FILAS_TOTALES; filaActual++) {
+            for (int numFila = 1; numFila <= filaActual; numFila++) {
+                System.out.print(numFila);
             }
-            for (int segundaMitad = primeraMitad - 1; segundaMitad >= 1; segundaMitad--) {
-                System.out.print(segundaMitad);
+            for (int numFila = filaActual - 1; numFila >= 1; numFila--) {
+                System.out.print(numFila);
             }
             System.out.println(); // * Salto de Línea
         }
