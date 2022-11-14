e89293ce1f21f17782f77f465e2f961c48292d11
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-14 09:11:44 +0100

    Cambio de estructura, anti 0 y TESTS

--- src/ejercicios/Ejercicio4.java
+++ src/ejercicios/Ejercicio4.java
@@ -1,3 +1,10 @@
+// TESTS
+// = Habrá que comprobar que no se pued3en introducir números negativos
+// = Si a es 4 y b es 10 el MCD deberá ser 2
+// = Si a es 50 y b es 100 el MCD deberá ser 50
+// = Si a es 15 y b es 70 el MCD deberá ser 5
+// = Si a o b son 0 o menor el programa te volverá a pedir los números
+
 package ejercicios;
 
 // ? Importamos la clase Scanner y Locale
@@ -7,7 +14,7 @@ public class Ejercicio4 {
     public static void main(String[] args) {
         // ? Programa Java el cual pida a y b al usuario y se realice el MCD de a y b
         // ^ Creamos las variables para a y b
-        final long a, b;
+        long a = 0, b = 0;
 
         // ^ Creamos una variable para el divisor y 2 para los módulos de a y b
         long divisor, moduloA = -1, moduloB = -1;
@@ -15,26 +22,28 @@ public class Ejercicio4 {
         // ^ Creamos un Scanner en la variable read y cambiamos su formato a US
         final Scanner read = new Scanner(System.in).useLocale(Locale.US);
 
-        // ! Pedimos a y b al usuario
-        System.out.print("Introduce a: ");
-        a = read.nextLong(); // * Para a
-        System.out.print("Introduce b: ");
-        b = read.nextLong(); // * Para b
-
+        while(a < 1){ // * a no puede ser 0 o menos
+            System.out.print("Introduce a: ");
+            a = read.nextLong(); // ! Pedimos a al usuario
+        } // * Repetiremos el bucle hasta que a sea mayor que 0
+        
+        while(b < 1){ // * b no puede ser 0 o menos
+            System.out.print("Introduce b: ");
+            b = read.nextLong(); // ! Pedimos b al usuario
+        } // * Repetiremos el bucle hasta que b sea mayor que 0
         // ? Salto de Línea Triple
         System.out.println("\n---------------------------------------\n");
 
         // ! Operador ternario para sobrescribir el valor de divisor
         divisor = (a < b) ? a : b; // * Si a es mayor que b, divisor es a, si no, divisor es b
 
-        // ! Mediante un bucle While vamos comprobando si a y b son divisibles entre el divisor
-        while (moduloA != 0 || moduloB != 0){
+        do { // ! Mediante un bucle do While vamos comprobando si a y b son divisibles entre el divisor
             // ! Comprobamos si a y b son divisibles entre el divisor
             moduloA = a % divisor; // * Calculamos el modulo de a
             moduloB = b % divisor; // * Calculamos el modulo de b
 
             divisor--; // ! Decrementamos el divisor
-        }
+        } while (moduloA != 0 || moduloB != 0); // * Repetiremos el bucle mientras a y b no sean divisibles entre el divisor
 
         // ! Imprimimos el MCD
         System.out.println("El máximo comun divisor de " + a + " y " + b + " es: " + (divisor + 1)); // * + 1 porque en While se decrementa el divisor
