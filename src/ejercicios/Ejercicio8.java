6f363d86065e0a2d9346f85b6294f534d2b7b1de
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-14 09:55:24 +0100

    Tests y programa mejorado

--- src/ejercicios/Ejercicio8.java
+++ src/ejercicios/Ejercicio8.java
@@ -1,3 +1,8 @@
+// TESTS
+// = Si introducimos 10, 9, 11 y 0 deberá dar 3 numeros introducidos y 1 fallo
+// = Si introducimos 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 y 0 deberá dar 10 numeros introducidos y 0 fallos
+// = Si introducimos 20, 21, 8, 15, 10, 30 y 0 deberá dar 6 numeros introducidos y 2 fallos
+
 package ejercicios;
 
 // ? Importamos la clase Scanner y Locale
@@ -19,28 +24,24 @@ public class Ejercicio8 {
         System.out.print("Introduce un numero inicial: ");
         n = read.nextLong();
 
-        
-        nMayor = n;
+        System.out.println(); // ? Salto de Línea
 
-        // ? Salto de Línea Triple
-        System.out.println("\n---------------------------------------\n");
+        nMayor = n; // * Hacemos que nMayor sea igual a n, el cual es el primer numero introducido por el usuario
 
         // ! Mediante un bucle while, pedimos los numeros al usuario
         while (n != 0){
-            // ! Pedimos el numero al usuario
-            System.out.print("Introduce un numero mayor que " + n + ": ");
-            n = read.nextLong();
+            System.out.print("Introduce un numero: ");
+            n = read.nextLong(); // ! Pedimos el numero al usuario
 
-            if (n > nMayor) nMayor = n; // * Si el numero es mayor que el anterior, lo guardamos en nMayor
+            if (n >= nMayor) nMayor = n; // * Si el numero es mayor que el anterior, lo guardamos en nMayor
 
-            // ! Incrementamos el contador
-            contador++;
+            contador++; // ! Incrementamos el contador
 
             // ! Si el numero es menor que el anterior, incrementamos el contador de fallos
             if (n < nMayor && n != 0){ 
                 System.out.println("El numero introducido es menor que el anterior"); // * Imprimimos un mensaje de error
                 fallos++; // * Incrementamos el contador de fallos
-                nMayor = 0; // * Reiniciamos nMayor
+                nMayor = n; // * Reiniciamos nMayor
             }
 
             // ? Salto de Línea Triple
