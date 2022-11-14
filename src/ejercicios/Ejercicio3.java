f169a4796e31998707b4967dd868cd6fb272347b
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-14 08:57:27 +0100

    Bucle anti 0 y TESTS

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -1,3 +1,10 @@
+// TESTS
+// = Habrá que comprobar que no se pued3en introducir números negativos
+// = Si altura = 4 el triangulo deberá ser:    *
+// =                                          * *
+// =                                         * * *
+// =                                        * * * *
+
 package ejercicios;
 
 // ? Importamos la clase Scanner y Locale
@@ -7,7 +14,7 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         // ? Programa que pida un numero al usuario y dibuje un triangulo en funcion de la altura introducida
         // ^ Creamos las variables para la altura y el caracter
-        short altura; // * Es short porque a partir de 1000 de altura no se puede imprimir bien por la consola
+        short altura = -1; // * Es short porque a partir de 1000 de altura no se puede imprimir bien por la consola yy 0 porque se va a comprabar que sea mayor que 0
         final char CHAR = '*'; // * Es final porque no se va a modificar una vez se le asigne un valor
 
         // ^ Creamos una variable para el retraso
@@ -16,9 +23,10 @@ public class Ejercicio3 {
         // ^ Creamos un Scanner en la variable read y cambiamos su formato a US
         final Scanner read = new Scanner(System.in).useLocale(Locale.US);
 
-        // ! Pedimos la altura al usuario
-        System.out.print("Introduce la altura del triangulo: ");
-        altura = read.nextShort();
+        while (altura < 1) { // * Debe ser mayor que 0 por lo que lo mínimo que puede introducir es 1
+            System.out.print("Introduce la altura del triángulo: ");
+            altura = read.nextShort(); // ! Pedimos la altura al usuario
+        } // * Si la altura es menor que 0, se volverá a pedir
 
         // ! Pedimos el retraso al usuario
         System.out.print("Introduce el delay (ms) ([0] para desactivarlo, [10] recomendado): ");
