7d6c0bf0267e7ea40c35a04fec0e906cdadf63ac
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-14 10:25:46 +0100

    Falta comentar bucle

--- src/ejercicios/Ejercicio7.java
+++ src/ejercicios/Ejercicio7.java
@@ -1,3 +1,10 @@
+// TESTS
+// = Habrá que comprobar que no se puede introducir numeros negativos
+// = Si se introduce 4 deberá quedar: 1
+// =                                  121
+// =                                  12321
+// =                                  1234321
+
 package ejercicios;
 
 // ? Importamos la clase Scanner y Locale
@@ -7,20 +14,21 @@ public class Ejercicio7 {
     public static void main(String[] args) {
         // ? Programa Java el cual imprima una piramide de n filas, los numeros deberan ser consecutivos empezando por 1, luego 121, 12321, 1234321, etc.
         // ^ Creamos la variable para filas
-        final long FILAS_TOTALES;
+        long filasTotales = 0;
 
         // ^ Creamos un Scanner en la variable read y cambiamos su formato a US
         final Scanner read = new Scanner(System.in).useLocale(Locale.US);
 
-        // ! Pedimos el numero al usuario
-        System.out.print("Introduce un numero entero positivo: ");
-        FILAS_TOTALES = read.nextInt();
+        while (filasTotales < 1) { // * filasTotales no puede ser negativo
+            System.out.print("Introduce el número de filas: ");
+            filasTotales = read.nextLong(); // ! Pedimos filasTotales al usuario
+        } // * Repetiremos el bucle hasta que filasTotales sea mayor que 0
 
         // ? Salto de Línea Triple
         System.out.println("\n---------------------------------------\n");
 
         // ! Imprimimos la piramide
-        for (int filaActual = 1; filaActual <= FILAS_TOTALES; filaActual++) {
+        for (int filaActual = 1; filaActual <= filasTotales; filaActual++) {
             for (int numFila = 1; numFila <= filaActual; numFila++) {
                 System.out.print(numFila);
             }
