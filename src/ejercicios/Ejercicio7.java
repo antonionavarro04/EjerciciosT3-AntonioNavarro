c898770307bdef59f9f26663c5f7a09fd721ff8a
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-15 12:19:47 +0100

    While a Do While y Comentarios Mejores

--- src/ejercicios/Ejercicio7.java
+++ src/ejercicios/Ejercicio7.java
@@ -14,15 +14,15 @@ public class Ejercicio7 {
     public static void main(String[] args) {
         // ? Programa Java el cual imprima una piramide de n filas, los numeros deberan ser consecutivos empezando por 1, luego 121, 12321, 1234321, etc.
         // ^ Creamos la variable para filas
-        long filasTotales = 0;
+        long filasTotales;
 
         // ^ Creamos un Scanner en la variable read y cambiamos su formato a US
         final Scanner read = new Scanner(System.in).useLocale(Locale.US);
 
-        while (filasTotales < 1) { // * filasTotales no puede ser negativo
-            System.out.print("Introduce el número de filas: ");
-            filasTotales = read.nextLong(); // ! Pedimos filasTotales al usuario
-        } // * Repetiremos el bucle hasta que filasTotales sea mayor que 0
+        do{
+            System.out.println("Introduce el numero de filas que quieres que tenga la piramide: ");
+            filasTotales = read.nextLong(); // ! Leemos el numero de filas
+        } while (filasTotales < 0); // * Comprobamos que el numero de filas sea mayor que 0
 
         // ? Salto de Línea Triple
         System.out.println("\n---------------------------------------\n");
@@ -30,10 +30,10 @@ public class Ejercicio7 {
         // ! Imprimimos la piramide
         for (int filaActual = 1; filaActual <= filasTotales; filaActual++) {
             for (int numFila = 1; numFila <= filaActual; numFila++) {
-                System.out.print(numFila);
+                System.out.print(numFila); // * Imprimimos el numero de la fila, en este bucle hará cuenta progresiva
             }
             for (int numFila = filaActual - 1; numFila >= 1; numFila--) {
-                System.out.print(numFila);
+                System.out.print(numFila); // * Imprimimos el numero de la fila, en este bucle hará cuenta regresiva
             }
             System.out.println(); // * Salto de Línea
         }
