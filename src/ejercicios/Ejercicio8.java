a1b092b955b8de9d5bcd12334fe6a594629ed839
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-15 12:24:51 +0100

    Cambio minimo en comentario

--- src/ejercicios/Ejercicio8.java
+++ src/ejercicios/Ejercicio8.java
@@ -11,8 +11,8 @@ import java.util.Scanner; import java.util.Locale;
 public class Ejercicio8 {
     public static void main(String[] args) {
         // ? Programa Java el cual lea solo los numeros mayores a el anterior introducido, la introduciión de números finaliza con un 0
-        // ^ Creamos la variable para n
-        long n, nMayor = 0;
+        // ^ Creamos la variable para n y nMayor
+        long n, nMayor;
 
         // ^ Creamos el contador y el contador de fallos
         int contador = 0, fallos = 0;
