fcb83db7e48b7e3967c3e89bb54ab64fa38c6a51
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-16 11:48:36 +0100

    Cambio a Do-While, Bucle Main Funciona y Comentarios Mejorados

--- src/ejercicios/Ejercicio5.java
+++ src/ejercicios/Ejercicio5.java
@@ -12,54 +12,41 @@ public class Ejercicio5 {
     public static void main(String[] args) {
         // ? Programa que dado a y b, calcule el MCM de a y b
         // ^ Creamos las variables para a y b
-        long a = 0, b = 0;
+        long a, b;
 
         // ^ Creamos una variable multiplicador y 2 para los resultados de a y b así como 2 para los multiplicadores de a y b
-        final long multiplicador;
-        long multiplicadorA, multiplicadorB, resultadoA, resultadoB;
+        long resultadoA, resultadoB, multiplicadorA = 1, multiplicadorB = 1; // * Las variables multiplicador se iniciaran a 1 para que en la multiplicación empieze desde el 1, ya qeu si el usuario introduce a = 2 y b = 2, el mcm será de 2 ya que son iguales
 
         // ^ Creamos un Scanner en la variable read y cambiamos su formato a US
         final Scanner read = new Scanner(System.in).useLocale(Locale.US);
 
-        while (a < 1) { // * a no puede ser 0 o menos
-            System.out.print("Introduce a: ");
-            a = read.nextLong(); // ! Pedimos a al usuario
-        } // * Repetiremos el bucle hasta que a sea mayor que 0
+        do{
+            System.out.print("Introduce el valor de a: ");
+            a = read.nextLong(); // ! Leemos el valor de a
+        } while (a <= 0); // * Si a es menor o igual a 0, se volverá a pedir el valor de a
         
-        while (b < 1) { // * b no puede ser 0 o menos
-            System.out.print("Introduce b: ");
-            b = read.nextLong(); // ! Pedimos b al usuario
-        } // * Repetiremos el bucle hasta que b sea mayor que 0
+        do{
+            System.out.print("Introduce el valor de b: ");
+            b = read.nextLong(); // ! Leemos el valor de b
+        } while (b <= 0); // * Si b es menor o igual a 0, se volverá a pedir el valor de b
 
         // ? Salto de Línea Triple
         System.out.println("\n---------------------------------------\n");
-        
-        // ! Mediante un operador ternario asignamos el valor de multiplicador
-        multiplicador = (a > b) ? a : b; // * Si a es mayor que b, multiplicador es a, si no, multiplicador es b
 
-        // ! Igualamos mA y mB a multiplicador
-        multiplicadorA = multiplicador; // * Para a
-        multiplicadorB = multiplicador; // * Para b
-
-        // ! Mediante un bucle While vamos comprobando si el resultado de a o b por el multiplicador son iguales
+        // ! Mediante un bucle Do-While multiplicaremos cada valor por su multiplicador
         do{
-            // ! Calculamos los resultados de x por el multiplicador          
-            resultadoA = a * multiplicadorA; // * Calculamos el resultado de a por el multiplicador
-            resultadoB = b * multiplicadorB; // * Calculamos el resultado de b por el multiplicador
+            // ! Calculamos los resultados de x por su multiplicador
+            resultadoA = a * multiplicadorA; // * Calculamos el resultado de a por el multiplicador de a
+            resultadoB = b * multiplicadorB; // * Calculamos el resultado de b por el multiplicador de b
             
-            // ! Mediante un if incereamos el multiplicador del que haya dado un resultado menor
-            if (resultadoA < resultadoB){ // * Para a
-                multiplicadorA++;
-            } else { // * Para b
-                multiplicadorB++;
-            }
+            // ! Mediante un if-else incrementamos el multiplicador del que haya dado un resultado menor
+            if (resultadoA < resultadoB) multiplicadorA++; // * Si a es menor que b, incrementamos solo el multiplicador de a
+            else multiplicadorB++; // * Si b es menor que a, incrementamos solo el multiplicador de b
+
         } while (resultadoA != resultadoB); // * Repetiremos el bucle hasta que los resultados sean iguales
 
         // ! Imprimimos el MCM
         System.out.println("El mínimo comun múltiplo de " + a + " y " + b + " es: " + resultadoA);
-        System.out.println(resultadoB);
-        System.out.println(multiplicadorA);
-        System.out.println(multiplicadorB);
 
         // ^ Cerramos el Scanner
         read.close();
